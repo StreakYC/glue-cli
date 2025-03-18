93a1f700377afbb773949a9b086418e72bd016d6
Aleem Mawani <aleem@streak.com>
2025-03-18 14:40:21 -0700

    fix prefix id regex

--- commands/describe.ts
+++ commands/describe.ts
@@ -53,8 +53,8 @@ function renderGlueAndDeployments(glue: GlueDTO, deployments: DeploymentDTO[], o
 }
 
 function isPrefixId(query: string, prefix: string) {
-  // prefix ids are like d_1800000000000000 or g_1800000000000000
-  // use a regex to check if the query starts with the prefix and is followed by 16 digits
-  const regex = new RegExp(`^${prefix}_[0-9]{16}$`);
+  // prefix ids are like d_34f4000000000000 or g_1800000000000000 or d_e18000000000000
+  // use a regex to check if the query starts with the prefix, then underscore then followed by any number of hex digits
+  const regex = new RegExp(`^${prefix}_[0-9a-f]{1,}$`);
   return regex.test(query);
 }
