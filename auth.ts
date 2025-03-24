83849fd639963618e1a9621e19117201f7d1c67c
Aleem Mawani <aleem@streak.com>
2025-03-24 13:41:59 -0700

    associate credentials with api server

--- auth.ts
+++ auth.ts
@@ -1,17 +1,18 @@
+import { GLUE_API_SERVER } from "./common.ts";
 import { kv } from "./db.ts";
 
 export const AUTH_TOKEN_KEY = "authToken";
 
 export async function setAuthToken(authToken: string) {
-  await kv.set([AUTH_TOKEN_KEY], authToken);
+  await kv.set([AUTH_TOKEN_KEY, GLUE_API_SERVER], authToken);
 }
 
 export async function getAuthToken() {
-  return (await kv.get<string>([AUTH_TOKEN_KEY])).value;
+  return (await kv.get<string>([AUTH_TOKEN_KEY, GLUE_API_SERVER])).value;
 }
 
 export async function clearAuthToken() {
-  await kv.delete([AUTH_TOKEN_KEY]);
+  await kv.delete([AUTH_TOKEN_KEY, GLUE_API_SERVER]);
 }
 
 export async function checkForAuthCredsOtherwiseExit() {
