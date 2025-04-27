44c3e513c913684579a2c3c768c4c8de4a40b56d
Aleem Mawani <aleem@streak.com>
2025-04-27 00:14:49 -0700

    simpler example

--- myExampleWebhook.ts
+++ myExampleWebhook.ts
@@ -1,8 +1,6 @@
 import { glue } from "jsr:@streak-glue/runtime";
 
 glue.github.onRepoEvent("StreakYC", "glue-backend", ["issues"], (event) => {
-  // deno-lint-ignore no-explicit-any
-  const payload = event.payload as any;
-  console.log(`event.payload.action: ${payload.action}`);
-  console.log(`event.payload.issue.title: ${payload.issue.title}`);
+  console.log(`event.payload.action: ${event.payload.action}`);
+  console.log(`event.payload.issue.title: ${event.payload.issue.title}`);
 });
