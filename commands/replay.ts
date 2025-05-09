608b0cc27f219da9fde57e5db7f2074e58e9f7e5
Aleem Mawani <aleem@streak.com>
2025-05-09 15:14:29 -0700

    fix linter

--- commands/replay.ts
+++ commands/replay.ts
@@ -2,7 +2,7 @@ import { runStep } from "../ui/utils.ts";
 import { checkForAuthCredsOtherwiseExit } from "../auth.ts";
 import { replayExecution } from "../backend.ts";
 
-export const replay = async (_options: Record<string, never>, executionId: string) => {
+export const replay = async (_options: unknown, executionId: string) => {
   await checkForAuthCredsOtherwiseExit();
   await runStep("Replaying execution...", () => replayExecution(executionId));
 };
