8f1cd4696f557f3e92c2e47aa4710f1d210218dd
Aleem Mawani <aleem@streak.com>
2025-05-15 23:34:50 -0700

    better UI for deploy

--- ui/deploy.tsx
+++ ui/deploy.tsx
@@ -1,6 +1,7 @@
 import type { BuildStepDTO, DeploymentDTO, StepStatus } from "../backend.ts";
 import React from "react";
 import { AuthTriggerList, BuildStepStatusRow, ClientStepRow, SetupTriggerList } from "./common.tsx";
+import { Newline, Text } from "ink";
 
 export type DeployUIProps = {
   codeAnalysisState: StepStatus;
@@ -13,6 +14,7 @@ export type DeployUIProps = {
 export const DeployUI = (
   { deployment, codeAnalysisState, codeAnalysisDuration, uploadingCodeState, uploadingCodeDuration }: DeployUIProps,
 ) => {
+  const done = deployment && deployment.buildSteps.every((step) => step.status === "success");
   return (
     <>
       <ClientStepRow stepState={codeAnalysisState} stepDuration={codeAnalysisDuration} stepTitle="Analyzing code" />
@@ -26,6 +28,16 @@ export const DeployUI = (
           {step.name === "triggerSetup" && step.status === "success" && <SetupTriggerList triggers={deployment.triggers} />}
         </React.Fragment>
       ))}
+      {done && (
+        <Text>
+          <Newline />
+          <Text>Successfully deployed your Glue 🎉</Text>
+          <Newline />
+          <Text color="dim">
+            Try `glue describe` to see summary info or `glue tail` to watch for executions
+          </Text>
+        </Text>
+      )}
     </>
   );
 };
