41987c2621ae9910a131dc6c29c298b70c186e89
Chris Cowan <agentme49@gmail.com>
2026-06-08 17:30:38 -0700

    Hide deploy runner option for now

--- glue.ts
+++ glue.ts
@@ -64,7 +64,7 @@ const cmd = new Command()
   .option(
     "-r, --runner <runner:runner>",
     "Use a specific runner to host the glue. Valid values are: deno, deno-v2, fly, cloudflare, sandbox.",
-    { default: "deno-v2" },
+    { default: "deno-v2", hidden: true },
   )
   .option("--tag <tag:string>", "Add tags to the glue (repeatable)", { collect: true })
   .option(
