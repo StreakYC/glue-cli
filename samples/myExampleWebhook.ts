c28b155d859fa5d826faf986d72149e829ee2ad4
Chris Cowan <agentme49@gmail.com>
2026-06-17 15:58:09 -0700

    revert sample changes

--- samples/myExampleWebhook.ts
+++ samples/myExampleWebhook.ts
@@ -1,8 +1,9 @@
 import { glue } from "jsr:@streak-glue/runtime";
 
-// const secret = glue.secrets.createSecretFetcher("foo3");
+glue.webhook.onGet((_event) => {
+  console.log(_event.bodyText);
+});
 
-glue.webhook.onGet(async (_event) => {
-  console.log("webhook triggered");
-  // console.log("value:", await secret.get());
+glue.webhook.onGet((_event) => {
+  console.log("webhook 2 triggered");
 });
